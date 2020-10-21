852b89d0e656124c1b01bc3b026848ae5f2f698e
silvermc <silvermc@lafayette.edu>
2020-10-21 11:15:10 -0400

    Algorithm stuff

---
+++ Algorithm.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.util.ArrayList;
+
+
+public class Algorithm {
+	public Algorithm(){
+		boolean max;
+	}
+	
+	
+	ArrayList<Board> findSuccessors(Board b, Player p){
+		ArrayList<Board> successors = new ArrayList<Board>();
+		int space = 0;
+		for(int i = 0; i < 8; i++) {
+			for(int j = 0; j < 8; j++) {
+				space++;
+				if(b.gameBoard[i][j] == p.color) {
+					if(j < 6 && b.gameBoard[i][j+1] == p.enemy && b.gameBoard[i][j+2] == " ") {
+						Board c = b.makeMove(space, space + 2);
+						c.value = c.evaluate();
+						successors.add(c);
+					}
+					if(j > 1 && b.gameBoard[i][j-1] == p.enemy && b.gameBoard[i][j-2] == " ") {
+						Board c = b.makeMove(space, space - 2);
+						c.value = c.evaluate();
+						successors.add(c);
+					}
+					if(i < 6 && b.gameBoard[i+1][j] == p.enemy && b.gameBoard[i+2][j] == " ") {
+						Board c = b.makeMove(space, space + 16);
+						c.value = c.evaluate();
+						successors.add(c);
+					}
+					if(i > 1 && b.gameBoard[i-1][j] == p.enemy && b.gameBoard[i-2][j] == " ") {
+						Board c = b.makeMove(space, space - 16);
+						c.value = c.evaluate();
+						successors.add(c);
+					}
+				}
+			}
+		}
+		return successors;
+	}
+}
---
+++ BoardTree.java
@@ -0,0 +1,12 @@
+
+public class BoardTree {
+	public BoardTree(Board h){
+		Board head = h;
+	}
+	
+	public void fillNodes(Board h, int depth) {
+		for(int i = 0; i < depth; i++) {
+			
+		}
+	}
+}
