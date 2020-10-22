3ca9e9b10f5bd9eab92db1d474800f170e5e58b5
silvermc <silvermc@lafayette.edu>
2020-10-22 10:49:12 -0400

    For Competing

---
+++ Controller.java
@@ -0,0 +1,13 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+
+public class Controller {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
